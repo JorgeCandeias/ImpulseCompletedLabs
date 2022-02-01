38d98cbc94e83449fcebbaf47a208150085a7ed6
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:08:01 +0000

    Lab 2

---
+++ Lab2/Impulse/Impulse.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿// long lived orleans client
+using Orleans;
+
+var orleans = new ClientBuilder()
+    .UseLocalhostClustering()
+    .Build();
+
+// connect to the cluster
+await orleans.Connect(async error =>
+{
+    Console.WriteLine(error.ToString());
+    await Task.Delay(TimeSpan.FromSeconds(1));
+    return true;
+});
+
+// ideally keep the client alive and reuse it until app shutdown
+Console.WriteLine("Connected!");
+
+// at app shutdown gracefully disconnect from the cluster
+await orleans.Close();
