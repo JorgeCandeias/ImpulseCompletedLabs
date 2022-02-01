8652fffed99dc8185af29021ddbcb438a1c90dd6
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:30:24 +0000

    Lab 19

---
+++ Lab19/Impulse/Impulse.Client/OrleansClientService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Providers;
+using Spectre.Console;
+
+internal class OrleansClientService : IHostedService
+{
+    public IClusterClient Client { get; }
+
+    public OrleansClientService(IHostEnvironment environment)
+    {
+        var builder = new ClientBuilder();
+
+        if (environment.IsDevelopment())
+        {
+            builder
+                .UseLocalhostClustering();
+        }
+        else
+        {
+            // todo
+        }
+
+        Client = builder
+            .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("Chat")
+            .Build();
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await AnsiConsole.Status().StartAsync("Connecting to server...", async ctx =>
+        {
+            ctx.Spinner(Spinner.Known.Dots);
+            ctx.Status = "Connecting...";
+
+            await Client.Connect(async error =>
+            {
+                AnsiConsole.MarkupLine("[bold red]Error:[/] error connecting to server!");
+                AnsiConsole.WriteException(error);
+
+                ctx.Status = "Waiting to retry...";
+
+                await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
+
+                ctx.Status = "Retrying connection...";
+                return true;
+            });
+
+            ctx.Status = "Connected!";
+        });
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await AnsiConsole.Status().StartAsync("Disconnecting...", async ctx =>
+        {
+            ctx.Spinner(Spinner.Known.Dots);
+            await Client.Close();
+        });
+    }
+}
---
+++ Lab19/Impulse/Impulse.Server/Program.cs
@@ -0,0 +1,85 @@
+﻿using Impulse.Chat;
+using Impulse.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Providers;
+using Orleans.Statistics;
+using System.Diagnostics;
+using System.Net;
+
+if (!TcpPortFinder.TryGetAvailablePort(11111, 11999, out var siloPort) ||
+    !TcpPortFinder.TryGetAvailablePort(30000, 30999, out var gatewayPort) ||
+    !TcpPortFinder.TryGetAvailablePort(40000, 40999, out var dashboardPort))
+{
+    throw new InvalidOperationException();
+}
+
+Console.Title = $"Silo: {siloPort}, Gateway: {gatewayPort}, Dashboard: {dashboardPort}";
+
+await Host
+    .CreateDefaultBuilder(args)
+    .UseEnvironment("Development")
+    .ConfigureLogging((context, logging) =>
+    {
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            // noop
+        }
+        else
+        {
+            logging.ClearProviders();
+        }
+    })
+    .UseOrleans((context, builder) =>
+    {
+        if (context.HostingEnvironment.IsDevelopment())
+        {
+            builder
+                .UseLocalhostClustering(siloPort, gatewayPort, new IPEndPoint(IPAddress.Loopback, 11111))
+                .AddMemoryGrainStorageAsDefault()
+                .AddMemoryGrainStorage("PubSubStore")
+                .UseInMemoryReminderService();
+        }
+        else
+        {
+            // todo
+        }
+
+        builder
+            .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("Chat")
+            .AddIncomingGrainCallFilter<ActivityGrainCallFilter>()
+            .UseDashboard(options =>
+            {
+                options.Port = dashboardPort;
+            });
+
+        if (OperatingSystem.IsWindows())
+        {
+            builder.AddPerfCountersTelemetryConsumer();
+        }
+        else if (OperatingSystem.IsLinux())
+        {
+            builder.UseLinuxEnvironmentStatistics();
+        }
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddOpenTelemetryTracing(builder =>
+        {
+            builder
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(nameof(Impulse)))
+                .AddSource(nameof(Impulse));
+
+            if (context.HostingEnvironment.IsDevelopment())
+            {
+                builder.AddConsoleExporter();
+            }
+        });
+        services.AddSingleton(sp => new ActivitySource(nameof(Impulse)));
+    })
+    .RunConsoleAsync();
