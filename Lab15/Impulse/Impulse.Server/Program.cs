a60bd34f5f03a7ada3b10247ee79f4f41c66cb77
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:21:23 +0000

    Lab 15

---
+++ Lab15/Impulse/Impulse.Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using Impulse.Chat;
+using Impulse.Server;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Providers;
+using System.Diagnostics;
+using System.Net;
+
+if (!TcpPortFinder.TryGetAvailablePort(11111, 11999, out var siloPort) ||
+    !TcpPortFinder.TryGetAvailablePort(30000, 30999, out var gatewayPort) ||
+    !TcpPortFinder.TryGetAvailablePort(40000, 40999, out var dashboardPort))
+{
+    throw new InvalidOperationException();
+}
+
+Console.Title = $"Silo: {siloPort}, Gateway: {gatewayPort}, Dashboard: {dashboardPort}";
+
+await Host.CreateDefaultBuilder(args)
+    .UseOrleans((context, builder) =>
+    {
+        builder
+            .UseLocalhostClustering(siloPort, gatewayPort, new IPEndPoint(IPAddress.Loopback, 11111))
+            .AddMemoryGrainStorageAsDefault()
+            .AddMemoryGrainStorage("PubSubStore")
+            .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("Chat")
+            .UseInMemoryReminderService()
+            .AddIncomingGrainCallFilter<ActivityGrainCallFilter>()
+            .UseDashboard(options =>
+            {
+                options.Port = dashboardPort;
+            });
+    })
+    .ConfigureServices(services =>
+    {
+        services.AddOpenTelemetryTracing(builder =>
+        {
+            builder
+                .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(nameof(Impulse)))
+                .AddSource(nameof(Impulse))
+                .AddConsoleExporter();
+        });
+        services.AddSingleton(sp => new ActivitySource(nameof(Impulse)));
+    })
+    .RunConsoleAsync();
