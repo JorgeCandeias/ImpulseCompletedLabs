63a7edc3dfd20ef297758c9c5addc7bbcec5c073
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:08:42 +0000

    Lab 3

---
+++ Lab3/Impulse/Impulse.Client/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services
+            .AddSingleton<OrleansClientService>()
+            .AddSingleton<IHostedService>(sp => sp.GetRequiredService<OrleansClientService>())
+            .AddSingleton(sp => sp.GetRequiredService<OrleansClientService>().Client);
+
+        services
+            .Configure<ConsoleLifetimeOptions>(options =>
+            {
+                options.SuppressStatusMessages = true;
+            });
+    })
+    .RunConsoleAsync();
