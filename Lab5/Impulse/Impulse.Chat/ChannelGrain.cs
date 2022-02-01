09fa70b8f9036d8a878ceb36d442da7782058f36
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:11:14 +0000

    Lab 5

---
+++ Lab5/Impulse/Impulse.Chat/ChannelGrain.cs
@@ -0,0 +1,55 @@
+﻿using Impulse.Models;
+using Orleans;
+using System.Collections.Immutable;
+
+namespace Impulse.Chat
+{
+    internal class ChannelGrain : Grain, IChannelGrain
+    {
+        private readonly Queue<ChatMessage> _messages = new();
+        public readonly HashSet<string> _members = new(StringComparer.OrdinalIgnoreCase);
+
+        public Task JoinAsync(string nickname)
+        {
+            _members.Add(nickname);
+
+            return Task.CompletedTask;
+        }
+
+        public Task LeaveAsync(string nickname)
+        {
+            _members.Remove(nickname);
+
+            return Task.CompletedTask;
+        }
+
+        public Task MessageAsync(ChatMessage message)
+        {
+            // cache the new message
+            _messages.Enqueue(message);
+
+            // clear any excess messages
+            while (_messages.Count > 100)
+            {
+                _messages.Dequeue();
+            }
+
+            // we cant broadcast the message yet...
+            return Task.CompletedTask;
+        }
+
+        public Task<ImmutableArray<string>> GetMembersAsync()
+        {
+            var result = _members.ToImmutableArray();
+
+            return Task.FromResult(result);
+        }
+
+        public Task<ImmutableArray<ChatMessage>> GetHistoryAsync()
+        {
+            var result = _messages.ToImmutableArray();
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ Lab5/Impulse/Impulse.Client/OrleansClientService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+using Orleans;
+
+internal class OrleansClientService : IHostedService
+{
+    public IClusterClient Client { get; }
+
+    public OrleansClientService()
+    {
+        Client = new ClientBuilder()
+            .UseLocalhostClustering()
+            .Build();
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Orleans Client connecting...");
+
+        await Client.Connect(async error =>
+        {
+            Console.WriteLine(error.ToString());
+            await Task.Delay(TimeSpan.FromSeconds(1), cancellationToken);
+            return true;
+        });
+
+        Console.WriteLine("Orleans Client connected!");
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await Client.Close();
+
+        Console.WriteLine("Orleans Client disconnected!");
+    }
+}
