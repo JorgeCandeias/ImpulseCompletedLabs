78c6e13991561c6bd3c76b6c6d7f87b491e827f8
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:18:04 +0000

    Lab 11

---
+++ Lab11/Impulse/Impulse.Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using Impulse.Server;
+using Microsoft.Extensions.Hosting;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Providers;
+using System.Net;
+
+if (!TcpPortFinder.TryGetAvailablePort(11111, 11999, out var siloPort) ||
+    !TcpPortFinder.TryGetAvailablePort(30000, 30999, out var gatewayPort) ||
+    !TcpPortFinder.TryGetAvailablePort(40000, 40999, out var dashboardPort))
+{
+    throw new InvalidOperationException();
+}
+
+Console.Title = $"Silo: {siloPort}, Gateway: {gatewayPort}, Dashboard: {dashboardPort}";
+
+await Host.CreateDefaultBuilder(args)
+    .UseOrleans((context, builder) =>
+    {
+        builder
+            .UseLocalhostClustering(siloPort, gatewayPort, new IPEndPoint(IPAddress.Loopback, 11111))
+            .AddMemoryGrainStorageAsDefault()
+            .AddMemoryGrainStorage("PubSubStore")
+            .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("Chat")
+            .UseDashboard(options =>
+            {
+                options.Port = dashboardPort;
+            });
+    })
+    .RunConsoleAsync();
---
+++ Lab11/Impulse/Impulse.Server/TcpPortFinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Net.Sockets;
+
+namespace Impulse.Server
+{
+    internal static class TcpPortFinder
+    {
+        public static bool TryGetAvailablePort(int start, int end, out int port)
+        {
+            var length = end - start;
+            for (var i = 0; i <= length; ++i)
+            {
+                port = start + i;
+                if (TryPort(port))
+                {
+                    return true;
+                }
+            }
+
+            port = -1;
+            return false;
+        }
+
+        private static bool TryPort(int port)
+        {
+            var listener = TcpListener.Create(port);
+            try
+            {
+                listener.ExclusiveAddressUse = true;
+                listener.Start();
+                return true;
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+            finally
+            {
+                listener.Stop();
+            }
+        }
+    }
+}
