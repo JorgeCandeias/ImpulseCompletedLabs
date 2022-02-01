7cb6bfaf18267d9f461c33e5a376578a26b2ff9f
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:25:09 +0000

    Lab 18

---
+++ Lab18/Impulse/Impulse.Chat.Tests/ClusterFixture.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Orleans;
+using Orleans.Hosting;
+using Orleans.Providers;
+using Orleans.TestingHost;
+using System;
+
+namespace Impulse.Chat.Tests
+{
+    public class ClusterFixture : IDisposable
+    {
+        public ClusterFixture()
+        {
+            Cluster = new TestClusterBuilder()
+                .AddSiloBuilderConfigurator<TestSiloConfigurator>()
+                .AddClientBuilderConfigurator<TestClientConfigurator>()
+                .Build();
+
+            Cluster.Deploy();
+        }
+
+        public void Dispose()
+        {
+            Cluster.StopAllSilos();
+            GC.SuppressFinalize(this);
+        }
+
+        public TestCluster Cluster { get; }
+    }
+
+    public class TestSiloConfigurator : ISiloConfigurator
+    {
+        public void Configure(ISiloBuilder siloBuilder)
+        {
+            siloBuilder
+                .AddMemoryGrainStorageAsDefault()
+                .AddMemoryGrainStorage("PubSubStore")
+                .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("Chat")
+                .UseInMemoryReminderService();
+        }
+    }
+
+    public class TestClientConfigurator : IClientBuilderConfigurator
+    {
+        public void Configure(IConfiguration configuration, IClientBuilder clientBuilder)
+        {
+            clientBuilder
+                .AddMemoryStreams<DefaultMemoryMessageBodySerializer>("Chat");
+        }
+    }
+}
---
+++ Lab18/Impulse/Impulse.Client/StreamObserver.cs
@@ -0,0 +1,36 @@
+﻿using Impulse.Models;
+using Orleans.Streams;
+using Spectre.Console;
+
+namespace Impulse.Client
+{
+    internal class StreamObserver : IAsyncObserver<ChatMessage>
+    {
+        private readonly string _channel;
+
+        public StreamObserver(string channel)
+        {
+            _channel = channel;
+        }
+
+        public Task OnCompletedAsync() => Task.CompletedTask;
+
+        public Task OnErrorAsync(Exception ex)
+        {
+            AnsiConsole.WriteException(ex);
+            return Task.CompletedTask;
+        }
+
+        public Task OnNextAsync(ChatMessage item, StreamSequenceToken token)
+        {
+            AnsiConsole.MarkupLine(
+                "[[[dim]{0}[/]]] [bold green]{1}[/] [bold yellow]{2}:[/] {3}",
+                item.Created.LocalDateTime,
+                _channel,
+                item.User,
+                item.Text);
+
+            return Task.CompletedTask;
+        }
+    }
+}
