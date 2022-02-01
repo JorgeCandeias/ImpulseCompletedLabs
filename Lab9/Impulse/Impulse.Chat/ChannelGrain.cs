8a0e0bca6adf9864c3c29461da3fb1add26182bf
jorge <jorge.candeias@protonmail.com>
2022-02-01 00:16:24 +0000

    Lab 9

---
+++ Lab9/Impulse/Impulse.Chat/ChannelGrain.cs
@@ -0,0 +1,88 @@
+﻿using Impulse.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Orleans;
+using System.Collections.Immutable;
+
+namespace Impulse.Chat
+{
+    internal class ChannelGrain : Grain, IChannelGrain
+    {
+        private readonly Queue<ChatMessage> _messages = new();
+        private readonly HashSet<string> _members = new(StringComparer.OrdinalIgnoreCase);
+        private readonly ILogger _logger;
+        private readonly ChannelOptions _options;
+
+        public ChannelGrain(ILogger<ChannelGrain> logger, IOptions<ChannelOptions> options)
+        {
+            _logger = logger;
+            _options = options.Value;
+        }
+
+        private string _name = null!;
+
+        public override Task OnActivateAsync()
+        {
+            _name = this.GetPrimaryKeyString();
+
+            _logger.LogInformation("{Grain}#{Key} activated", nameof(ChannelGrain), _name);
+
+            return base.OnActivateAsync();
+        }
+
+        public override Task OnDeactivateAsync()
+        {
+            _logger.LogInformation("{Grain}#{Key} deactivated", nameof(ChannelGrain), _name);
+
+            return base.OnDeactivateAsync();
+        }
+
+        public Task JoinAsync(string nickname)
+        {
+            _members.Add(nickname);
+
+            return Task.CompletedTask;
+        }
+
+        public Task LeaveAsync(string nickname)
+        {
+            _members.Remove(nickname);
+
+            if (_members.Count == 0)
+            {
+                DeactivateOnIdle();
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task MessageAsync(ChatMessage message)
+        {
+            // cache the new message
+            _messages.Enqueue(message);
+
+            // clear any excess messages
+            while (_messages.Count > _options.MaxCachedMessages)
+            {
+                _messages.Dequeue();
+            }
+
+            // we cant broadcast the message yet...
+            return Task.CompletedTask;
+        }
+
+        public Task<ImmutableArray<string>> GetMembersAsync()
+        {
+            var result = _members.ToImmutableArray();
+
+            return Task.FromResult(result);
+        }
+
+        public Task<ImmutableArray<ChatMessage>> GetHistoryAsync()
+        {
+            var result = _messages.ToImmutableArray();
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ Lab9/Impulse/Impulse.Client/OrleansClientService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using Orleans;
+using Spectre.Console;
+
+internal class OrleansClientService : IHostedService
+{
+    public IClusterClient Client { get; }
+
+    public OrleansClientService()
+    {
+        Client = new ClientBuilder()
+            .UseLocalhostClustering()
+            .Build();
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await AnsiConsole.Status().StartAsync("Connecting to server...", async ctx =>
+        {
+            ctx.Spinner(Spinner.Known.Dots);
+            ctx.Status = "Connecting...";
+
+            await Client.Connect(async error =>
+            {
+                AnsiConsole.MarkupLine("[bold red]Error:[/] error connecting to server!");
+                AnsiConsole.WriteException(error);
+
+                ctx.Status = "Waiting to retry...";
+
+                await Task.Delay(TimeSpan.FromSeconds(2), cancellationToken);
+
+                ctx.Status = "Retrying connection...";
+                return true;
+            });
+
+            ctx.Status = "Connected!";
+        });
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await AnsiConsole.Status().StartAsync("Disconnecting...", async ctx =>
+        {
+            ctx.Spinner(Spinner.Known.Dots);
+            await Client.Close();
+        });
+    }
+}
